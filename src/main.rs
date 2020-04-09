ee4677beefb0da3d5c70a2e23b51780ff7619e77
dwij patel <saurabh.sachit39@gmail.com>
2020-04-09 19:38:49 +0530

    screen shot done

--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,39 @@
 extern crate rand;
-
 use std::{thread, time, env, process::Command};
 use rand::Rng;
 
 fn main(){
-    generate_output(String::from("/home/ddl/wow.png"),String::from("ls"));
-  
+    create_output(String::from("pwd"),String::from("/home/ddl/work.png"))
 }
 
+fn create_output(command:String,path:String){
 
-fn generate_output(path:String,command:String){
+        let mut rng = rand::thread_rng();
 
-    let mut rng = rand::thread_rng();
-
-    let  display_number:u8=rng.gen();
-
-  
+        let  display_number:u8=rng.gen();
    
-       
-    Command::new("sh")
-                .arg("-c")
-                .arg(format!("Xvfb :{} -screen 5 100x100x8 &",&display_number))
-                .spawn()
-                .expect("failed to execute process");
+        Command::new("sh")
+            .arg("-c")
+            .arg(format!("Xvfb :{} -screen 5 100x100x8 &",&display_number))
+            .spawn()
+            .expect("failed to spawn");
+            
 
-    let mut i=0;
-    loop{
-        if(i==100){
-            break;
-        }
-        i=i+100;
-    }
+        thread::sleep(time::Duration::from_millis(1000));
+            
+        Command::new("sh")
+                    .env("DISPLAY", format!(":{}",&display_number))
+                    .arg("-c")
+                    .arg(format!("xterm -hold -fa monaco -fs 25 -bg black -fg green -maximized -e {} &",command))
+                    .spawn()
+                    .expect("failed to spawn");
+           
+        thread::sleep(time::Duration::from_millis(1000));
 
-    Command::new("sh")
-                .env("DISPLAY", format!(":{}",&display_number))
-                .arg("-c")
-                .arg(format!("xterm -hold -maximized -e {} &",command))
-                .spawn()
-                .expect("failed to execute process");
-
- 
-
-    Command::new("sh")
-                .arg("-c")
-                .arg(format!("xwd -display :{} -root -silent | convert xwd:- png:{}",&display_number,path))
-                .spawn()
-                .expect("failed to execute process");
+        Command::new("sh")
+                    .arg("-c")
+                    .arg(format!("xwd -display :{} -root -silent | convert xwd:- png:{}",&display_number,path))
+                    .spawn()
+                    .expect("failed to spawn");     
 }
+
